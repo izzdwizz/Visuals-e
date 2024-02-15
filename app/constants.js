d35f86ce5487cd643de94b27216c12e56cc20b85
Izuchukwu Uchendu <izuchukwu.uchendu@gmail.com>
2024-02-15 16:14:45 +0100

    First commit

---
+++ app/constants.js
@@ -0,0 +1,72 @@
+import image2 from '@/public/pic2.jpg';
+import image3 from '@/public/pic3.jpg';
+import image4 from '@/public/pic4.jpg';
+import image5 from '@/public/choco.jpg';
+import image6 from '@/public/pic5.jpg';
+
+export const generalview = [
+	{
+		id: 1,
+		photo: image2,
+		alt: 'Feature Icon with circle',
+		heading: 'Web Security',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.',
+	},
+	{
+		id: 2,
+		photo: image3,
+		alt: 'Feature Icon with circle',
+		heading: 'Email Security',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.',
+	},
+	{
+		id: 3,
+		photo: image4,
+		alt: 'Feature Icon with circle',
+		heading: 'Endpoint Security',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat',
+	},
+	{
+		id: 4,
+		photo: image5,
+		alt: 'Feature Icon with circle',
+		heading: 'Firewall & ADR',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat',
+	},
+	{
+		id: 5,
+		photo: image6,
+		alt: 'Feature Icon with circle',
+		heading: 'Firewall & ADR',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat',
+	},
+	{
+		id: 6,
+		photo: image4,
+		alt: 'Feature Icon with circle',
+		heading: 'Endpoint Security',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat',
+	},
+	{
+		id: 7,
+		photo: image5,
+		alt: 'Feature Icon with circle',
+		heading: 'Firewall & ADR',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat',
+	},
+	{
+		id: 8,
+		photo: image6,
+		alt: 'Feature Icon with circle',
+		heading: 'Firewall & ADR',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat',
+	},
+];
---
+++ next.config.mjs
@@ -0,0 +1,4 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = { outputFileTracing: false };
+
+export default nextConfig;
